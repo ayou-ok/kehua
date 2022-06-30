970b6db2809ec7c3476fc6b4dee9c90faf4d5e4b
阿佑 <ayooooo@petalmail.com>
2022-06-30 22:00:28 +0800

    axis improve

--- src/axis/mainAxis.ts
+++ src/axis/mainAxis.ts
@@ -7,51 +7,136 @@
 
 type Range = [from: number, to: number]
 
-function mainAxis () {
+type OverflowMode = 'left' | 'right' | 'none'
+
+type Domain = (string | number)[]
+
+export type MainAxisOptions = Partial<{
+  overflowModel: OverflowMode;
+  range: Range;
+  domain: Domain;
+  bandWidth: number;
+  padding: number;
+}>
+
+function mainAxis (options: MainAxisOptions = {}) {
   const FROM = 0
   const TO = 1
 
-  let range: Range = [0, 1]
-  let domain: Range = [0, 1]
-  let renagedDomain: Range = [0, 1]
-  let unitDromainRange = 1
-  let originDomain = 0
+  let overflowMode: OverflowMode = options.overflowModel ?? 'left'
+  let _bandWidth = options.bandWidth ?? 1
+  let _range: Range = options.range ?? [0, 1]
+  let _domain: Domain = options.domain ?? [0, 1]
+  let _padding = options.padding ?? 0
+  let unitDomainWorth = 1
+  let visiableRange: Range = [0, 1]
+  let visiableDomain: Range = [0, 1]
 
   function axis(domain: number) {
     return domain * 1
   }
 
+  /**
+   * 更新内部变量
+   */
   function apply() {
-    unitDromainRange = (range[TO] - range[FROM]) / (domain[TO] - domain[FROM])
+    const step = (_range[TO] - _range[FROM]) / _domain.length
+    _bandWidth = (1 - _padding) * step
   }
 
-  function domain(nextDomain?: Range) {
+  /**
+   * 设置/获取domain(轴上的值)
+   * @param nextDomain
+   * @returns 
+   */
+  function domain(nextDomain?: Domain) {
     if (nextDomain) {
-      domain = nextDomain
+      _domain = nextDomain
       apply()
     }
 
     return domain
   }
 
-  function padding () {}
+  /**
+   * 
+   * @param nextPadding 0 - 1 之间的数字
+   * @returns 
+   */
+  function padding (nextPadding?: number) {
+    if (nextPadding && nextPadding > 0 && nextPadding <=1) {
+      _padding = nextPadding
+      apply()
+    }
 
-  function step () {}
+    return _padding
+  }
 
-  function bandWidth () {}
+  /**
+   * 获取一个band的区域宽度
+   */
+  function step () {
+    return _bandWidth * (1 + _padding)
+  }
 
-  function scaleTo () {}
+  /**
+   * 设置/获取一个band宽度
+   * @param nextBandWith 
+   * @returns 
+   */
+  function bandWidth (nextBandWith?: number) {
+    if (nextBandWith) {
+      _bandWidth = nextBandWith
+      apply()
+    }
 
-  function rangeTo () {}
+    return _bandWidth
+  }
 
+  /**
+   * 设置/读取range(css区域)
+   * @param nextRange 
+   * @returns 
+   */
   function range(nextRange?: Range) {
     if (nextRange) {
-      range = nextDomain
+      _range = nextRange
       apply()
     }
 
     return range
   }
+
+  /**
+   * 平移轴
+   * @param x 
+   */
+  function translate (x: number) {
+    return x
+  }
+
+  /**
+   * 缩放轴
+   * @param k 缩放系数
+   * @param px 缩放点
+   */
+  function scale (k: number, px?: number) {
+    return k * (px ?? 1)
+  }
+
+  /**
+   * domain选区
+   * @param start start domain
+   * @param stop  stop domain
+   */
+  function clip (start: number, stop?: number) {}
+
+  /**
+   * range选区
+   * @param from range from
+   * @param to  range stop
+   */
+  function clipRange (from: number, to?: number) {}
 
   axis.prototype = {
     domain,
@@ -59,6 +144,8 @@ function mainAxis () {
     padding,
     step,
     bandWidth,
+    translate,
+    scale,
   }
 
   return axis
